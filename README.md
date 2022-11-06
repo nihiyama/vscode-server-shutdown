96b4c03551994700d89646e9d1219b6554d6914b
nihiyama <1klyot8qo@gmail.com>
2022-11-06 21:01:03 +0900

    add ssh

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# VSCode server shutdown
+# VSCode Server Shutdown
 
 This is a shellscript for automatically shutdown the VSCode server when there is no connection from the client like AWS cloud9.
 
 If the client opens VSCode using a web browser, it will not be shut down. But if the browser（the page where VSCode is open) is closed, the server will be automatically shut down 30 minutes after it is closed.
+
+## VSCode Server Shutdown for HTTP
+
+- vscode_server_shutdown_http.sh
+
+## VSCode Server Shutdown for SSH
+
+- vscode_server_shutdown_ssh.sh
--- vscode_server_shutdown_http.sh
+++ vscode_server_shutdown_http.sh
@@ -10,7 +10,7 @@ while true; do
     connection_established_process=`netstat -anp | grep ESTABLISHED | grep node | awk '{print $7}'`
     is_connected=false
     for e_p_ in ${connection_established_process}; do
-	    for v_p_ in ${vscode_server_process}; do
+        for v_p_ in ${vscode_server_process}; do
             if [[ "${e_p_}" =~ "${v_p_}"  ]]; then
                 # connection established process is pid/node.
                 # confirm that established process contains vscoder server process (node).
@@ -23,8 +23,8 @@ while true; do
     else
         connection_not_established_time=$((${connection_not_established_time} + ${SLEEP_TIME}))
     fi
-	if [ ${connection_not_established_time} -ge ${SHUTDOWN_TIME} ]; then
+    if [ ${connection_not_established_time} -ge ${SHUTDOWN_TIME} ]; then
         echo "client did not connect to vscode server... bye bye.."
         shutdown -h now
-	fi
+    fi
 done
